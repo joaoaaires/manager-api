dcdc0e2062cc2dc3d341dc1abb9d08fbd75aed28
João Antonio Aires <joaoaaires@MacBook-Air-de-Joao.local>
2026-02-20 17:21:44 -0300

    config banco de dados

---
+++ src/modules/user/user.repository.ts
@@ -0,0 +1,15 @@
+import { Repository } from 'typeorm';
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { UserEntity } from './entities/user.entity';
+
+@Injectable()
+export class UserRepository extends Repository<UserEntity> {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly repository: Repository<UserEntity>,
+  ) {
+    super(repository.target, repository.manager, repository.queryRunner);
+  }
+}
